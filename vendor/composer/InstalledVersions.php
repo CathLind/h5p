1641f88bc97c11a5a1dd06f9821e759fb8e151f7
Cath Lind <66523094+CathLind@users.noreply.github.com>
2021-08-05 11:20:11 +0200

    Add files via upload

---
+++ vendor/composer/InstalledVersions.php
@@ -0,0 +1,303 @@
+<?php
+
+
+
+
+
+
+
+
+
+
+
+namespace Composer;
+
+use Composer\Autoload\ClassLoader;
+use Composer\Semver\VersionParser;
+
+
+
+
+
+
+
+
+class InstalledVersions
+{
+private static $installed = array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-8.x-1.x',
+    'version' => 'dev-8.x-1.x',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '61eb9eda5fdab967fe94d6f39c7d9b98c76642fd',
+    'name' => 'drupal/h5p',
+  ),
+  'versions' => 
+  array (
+    'drupal/h5p' => 
+    array (
+      'pretty_version' => 'dev-8.x-1.x',
+      'version' => 'dev-8.x-1.x',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '61eb9eda5fdab967fe94d6f39c7d9b98c76642fd',
+    ),
+    'h5p/h5p-core' => 
+    array (
+      'pretty_version' => '1.24.2',
+      'version' => '1.24.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'db3da7a1441ae6c9ffacbb8110f41758c85beaa0',
+    ),
+    'h5p/h5p-editor' => 
+    array (
+      'pretty_version' => '1.24.3',
+      'version' => '1.24.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '338ae5bad572fe3d7c0a355d8ba8e24f360bb698',
+    ),
+  ),
+);
+private static $canGetVendors;
+private static $installedByVendor = array();
+
+
+
+
+
+
+
+public static function getInstalledPackages()
+{
+$packages = array();
+foreach (self::getInstalled() as $installed) {
+$packages[] = array_keys($installed['versions']);
+}
+
+if (1 === \count($packages)) {
+return $packages[0];
+}
+
+return array_keys(array_flip(\call_user_func_array('array_merge', $packages)));
+}
+
+
+
+
+
+
+
+
+
+public static function isInstalled($packageName)
+{
+foreach (self::getInstalled() as $installed) {
+if (isset($installed['versions'][$packageName])) {
+return true;
+}
+}
+
+return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public static function satisfies(VersionParser $parser, $packageName, $constraint)
+{
+$constraint = $parser->parseConstraints($constraint);
+$provided = $parser->parseConstraints(self::getVersionRanges($packageName));
+
+return $provided->matches($constraint);
+}
+
+
+
+
+
+
+
+
+
+
+public static function getVersionRanges($packageName)
+{
+foreach (self::getInstalled() as $installed) {
+if (!isset($installed['versions'][$packageName])) {
+continue;
+}
+
+$ranges = array();
+if (isset($installed['versions'][$packageName]['pretty_version'])) {
+$ranges[] = $installed['versions'][$packageName]['pretty_version'];
+}
+if (array_key_exists('aliases', $installed['versions'][$packageName])) {
+$ranges = array_merge($ranges, $installed['versions'][$packageName]['aliases']);
+}
+if (array_key_exists('replaced', $installed['versions'][$packageName])) {
+$ranges = array_merge($ranges, $installed['versions'][$packageName]['replaced']);
+}
+if (array_key_exists('provided', $installed['versions'][$packageName])) {
+$ranges = array_merge($ranges, $installed['versions'][$packageName]['provided']);
+}
+
+return implode(' || ', $ranges);
+}
+
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+
+
+
+
+public static function getVersion($packageName)
+{
+foreach (self::getInstalled() as $installed) {
+if (!isset($installed['versions'][$packageName])) {
+continue;
+}
+
+if (!isset($installed['versions'][$packageName]['version'])) {
+return null;
+}
+
+return $installed['versions'][$packageName]['version'];
+}
+
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+
+
+
+
+public static function getPrettyVersion($packageName)
+{
+foreach (self::getInstalled() as $installed) {
+if (!isset($installed['versions'][$packageName])) {
+continue;
+}
+
+if (!isset($installed['versions'][$packageName]['pretty_version'])) {
+return null;
+}
+
+return $installed['versions'][$packageName]['pretty_version'];
+}
+
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+
+
+
+
+public static function getReference($packageName)
+{
+foreach (self::getInstalled() as $installed) {
+if (!isset($installed['versions'][$packageName])) {
+continue;
+}
+
+if (!isset($installed['versions'][$packageName]['reference'])) {
+return null;
+}
+
+return $installed['versions'][$packageName]['reference'];
+}
+
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+
+
+
+
+public static function getRootPackage()
+{
+$installed = self::getInstalled();
+
+return $installed[0]['root'];
+}
+
+
+
+
+
+
+
+public static function getRawData()
+{
+return self::$installed;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public static function reload($data)
+{
+self::$installed = $data;
+self::$installedByVendor = array();
+}
+
+
+
+
+
+private static function getInstalled()
+{
+if (null === self::$canGetVendors) {
+self::$canGetVendors = method_exists('Composer\Autoload\ClassLoader', 'getRegisteredLoaders');
+}
+
+$installed = array();
+
+if (self::$canGetVendors) {
+foreach (ClassLoader::getRegisteredLoaders() as $vendorDir => $loader) {
+if (isset(self::$installedByVendor[$vendorDir])) {
+$installed[] = self::$installedByVendor[$vendorDir];
+} elseif (is_file($vendorDir.'/composer/installed.php')) {
+$installed[] = self::$installedByVendor[$vendorDir] = require $vendorDir.'/composer/installed.php';
+}
+}
+}
+
+$installed[] = self::$installed;
+
+return $installed;
+}
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-8.x-1.x',
+    'version' => 'dev-8.x-1.x',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '61eb9eda5fdab967fe94d6f39c7d9b98c76642fd',
+    'name' => 'drupal/h5p',
+  ),
+  'versions' => 
+  array (
+    'drupal/h5p' => 
+    array (
+      'pretty_version' => 'dev-8.x-1.x',
+      'version' => 'dev-8.x-1.x',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '61eb9eda5fdab967fe94d6f39c7d9b98c76642fd',
+    ),
+    'h5p/h5p-core' => 
+    array (
+      'pretty_version' => '1.24.2',
+      'version' => '1.24.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'db3da7a1441ae6c9ffacbb8110f41758c85beaa0',
+    ),
+    'h5p/h5p-editor' => 
+    array (
+      'pretty_version' => '1.24.3',
+      'version' => '1.24.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '338ae5bad572fe3d7c0a355d8ba8e24f360bb698',
+    ),
+  ),
+);
