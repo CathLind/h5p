c9fa35dec7d14f30af73c55e1907ebe04dd844dd
Cath Lind <66523094+CathLind@users.noreply.github.com>
2021-08-06 16:33:53 +0200

    Update H5PAdminSettingsForm.php

--- src/Form/H5PAdminSettingsForm.php
+++ src/Form/H5PAdminSettingsForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\h5p\Form;
 
+use Drupal\Core\File\FileSystemInterface;
 use Drupal\h5p\H5PDrupal\H5PDrupal;
 
 use Drupal\Core\Form\FormBase;
@@ -204,10 +205,10 @@ class H5PAdminSettingsForm extends FormBase {
     $path = \Drupal::service('file_system')->realpath('public://') . '/' . $h5p_default_path;
     $temp_path = $path . '/' . 'temp';
 
-    if (! file_prepare_directory($path, FILE_CREATE_DIRECTORY | FILE_MODIFY_PERMISSIONS)) {
+    if (! \Drupal::service('file_system')->prepareDirectory($path, FileSystemInterface::CREATE_DIRECTORY | FileSystemInterface::MODIFY_PERMISSIONS)) {
       $form_state->setErrorByName('h5p_default_path', t('You have specified an invalid directory.'));
     }
-    if (! file_prepare_directory($temp_path, FILE_CREATE_DIRECTORY | FILE_MODIFY_PERMISSIONS)) {
+    if (! \Drupal::service('file_system')->prepareDirectory($temp_path, FileSystemInterface::CREATE_DIRECTORY | FileSystemInterface::MODIFY_PERMISSIONS)) {
       $form_state->setErrorByName('h5p_default_path', t('You have specified an invalid directory.'));
     }
 
